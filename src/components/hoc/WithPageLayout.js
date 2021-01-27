73504bbc3da28e52976053c3beca0ccdc0074a46
ashley-woodton <ashley.woodton@gmail.com>
2021-01-27 00:20:49 -0500

    page layout fix

--- src/components/hoc/WithPageLayout.js
+++ src/components/hoc/WithPageLayout.js
@@ -1,4 +1,4 @@
-import React from 'https://cdn.skypack.dev/react';
+import React from 'react';
 import Shipping from '../partials/shipping/Shipping';
 import NavBar from '../partials/navbar/Navbar';
 import Footer from "../partials/footer/Footer";
