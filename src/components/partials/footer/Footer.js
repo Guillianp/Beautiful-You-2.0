9f2e19056f0a1850111924b6e960bc50e1b6956e
ashley-woodton <ashley.woodton@gmail.com>
2021-02-04 07:59:28 -0500

    responsiveness

--- src/components/partials/footer/Footer.js
+++ src/components/partials/footer/Footer.js
@@ -4,27 +4,27 @@ import logo from '../../../img/BEAUTIFUL.png';
 
 const Footer = () => (
     <footer>
-              <div class="footer-container">
-                  <div class="left-col">
-                      <img src={logo} alt="Beautiful You Logo" class="logo"/>
-                      <div class="social-media">
+              <div className="footer-container">
+                  <div className="left-col">
+                      <img src={logo} alt="Beautiful You Logo" className="logo"/>
+                      <div className="social-media">
                           <a href="https://www.instagram.com" target="_blank" rel="noopener"><i title="Instagram" class="fab fa-instagram"></i></a>
                           <a href="https://www.facebook.com" target="_blank" rel="noopener"><i title="Facebook" class="fab fa-facebook"></i></a>
                           <a href="https://www.youtube.com" target="_blank" rel="noopener"><i title="Youtube" class="fab fa-youtube"></i></a>
                       </div>
-                      <p class="copyrights">&#169; 2020 Beautiful You</p>
+                      <p className="copyrights">&#169; 2020 Beautiful You</p>
                   </div>
-                  <div class="right-col">
+                  <div className="right-col">
                       <h1>WANT 15% OFF?</h1>
-                      <div class="border"></div>
+                      <div className="border"></div>
                       <p>Sign up for our email list and recieve 15% off your first order.</p>
-                      <form class="foot-discount">
+                      <form className="foot-discount">
                           <label>
                               Email
-                          <input name="email" type="email" class="foot-email"
+                          <input name="email" type="email" className="foot-email"
                           required placeholder="Enter Your Email"/>
                           </label><br/>
-                          <input type="submit" class="btn" value="submit"/>
+                          <input type="submit" className="btn" value="submit"/>
                       </form>
                   </div>
               </div>
